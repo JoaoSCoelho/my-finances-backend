5c99a52e7216211117fe55853bb1bfc1a82faffa
João Victor Coelho <joaovictor-san-co@hotmail.com>
2023-03-05 20:06:18 -0300

    configure jest

---
+++ tests/jest.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { resolve } = require('path');
+const root = resolve(__dirname, '..');
+const rootConfig = require(`${root}/jest.config.js`);
+
+module.exports = {
+  ...rootConfig,
+  ...{
+    rootDir: root,
+    displayName: 'end2end-tests',
+    setupFilesAfterEnv: ['<rootDir>/tests/jest-setup.ts'],
+    testMatch: ['<rootDir>/tests/**/*.test.ts'],
+  },
+};
