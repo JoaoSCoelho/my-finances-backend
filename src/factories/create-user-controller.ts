0599246ecb55cbe8be6c113edadb3b0516089817
João Victor Coelho <joaovictor-san-co@hotmail.com>
2023-03-12 07:46:53 -0300

    Update create-user-controller.ts

--- src/factories/create-user-controller.ts
+++ src/factories/create-user-controller.ts
@@ -2,6 +2,8 @@ import { CreateUserController } from '../adapters/controllers/create-user-contro
 import { Bcrypt } from '../external/encryptor-providers/bcrypt';
 import { Moment } from '../external/generator-id-providers/moment';
 import { MongoUsers } from '../external/repositories/users/mongodb';
+import { Jwt } from '../external/token-manager/jwt';
+import { CreateAuthTokenUC } from '../use-cases/create-auth-token';
 import { CreateUserUC } from '../use-cases/create-user';
 
 export function makeCreateUserController(): CreateUserController {
@@ -13,7 +15,12 @@ export function makeCreateUserController(): CreateUserController {
     generatorID,
     usersRepository,
   );
-  const createUserController = new CreateUserController(createUserUC);
+  const tokenManager = new Jwt();
+  const createUserAuthTokenUC = new CreateAuthTokenUC(tokenManager);
+  const createUserController = new CreateUserController(
+    createUserUC,
+    createUserAuthTokenUC,
+  );
 
   return createUserController;
 }
